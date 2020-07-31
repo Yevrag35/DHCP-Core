fd4530bf9c2c6b7c264d6cb9902b510f661ce590
Mike Garvey <mike.garvey@yevrag35.com>
2020-07-31 17:37:39 -0400

    Add 'IsSet' property to DnsCredential

--- MG.Core.Dhcp/Models/DhcpServerDnsCredential.cs
+++ MG.Core.Dhcp/Models/DhcpServerDnsCredential.cs
@@ -6,6 +6,8 @@ namespace MG.Core.PowerShell.Dhcp.Models
 {
     public class DhcpServerDnsCredential
     {
+        public bool IsSet => !string.IsNullOrWhiteSpace(this.Domain) && !string.IsNullOrWhiteSpace(this.UserName);
+
         [CimProperty("DomainName")]
         public string Domain { get; private set; }
 
